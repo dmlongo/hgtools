35b5c9ab97758d67822cde74d963c7980002d562
david <david@192.168.0.241>
2019-11-21 16:41:41 +0100

    MainSQL fixed.

--- hgtools/src/at/ac/tuwien/dbai/hgtools/MainSQL.java
+++ hgtools/src/at/ac/tuwien/dbai/hgtools/MainSQL.java
@@ -4,52 +4,34 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
+import java.util.LinkedList;
 
 import at.ac.tuwien.dbai.hgtools.hypergraph.Hypergraph;
-import at.ac.tuwien.dbai.hgtools.sql2hg.old.BasePredicate;
-import at.ac.tuwien.dbai.hgtools.sql2hg.old.HypergraphFromSQLFinder;
-import at.ac.tuwien.dbai.hgtools.sql2hg.old.HypergraphFromSQLHelper;
-import at.ac.tuwien.dbai.hgtools.sql2hg.old.SameColumn;
-import at.ac.tuwien.dbai.hgtools.sql2hg.old.Schema;
+import at.ac.tuwien.dbai.hgtools.sql2hg.ConjunctiveQueryFinder;
+import at.ac.tuwien.dbai.hgtools.sql2hg.Equality;
+import at.ac.tuwien.dbai.hgtools.sql2hg.HypergraphBuilder;
+import at.ac.tuwien.dbai.hgtools.sql2hg.Predicate;
+import at.ac.tuwien.dbai.hgtools.sql2hg.PredicateDefinition;
+import at.ac.tuwien.dbai.hgtools.sql2hg.Schema;
+import at.ac.tuwien.dbai.hgtools.sql2hg.ViewPredicate;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
-import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.Statements;
 import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
 import net.sf.jsqlparser.statement.select.Select;
 
-
 public class MainSQL {
 
 	public static void main(String[] args) throws JSQLParserException {
 		Schema schema = new Schema();
-		
 		String schemaString = readFile(args[0]);
-		
-		Statements schemaStmts = CCJSqlParserUtil.parseStatements(schemaString);
-		 
-		for ( Statement schemaStmt : schemaStmts.getStatements() ) {
-			try {
-			   CreateTable tbl = (CreateTable) schemaStmt;
-			
-			   
-			   //System.out.println("Table: "+tbl.getTable().getName());
-			   BasePredicate p = new BasePredicate(tbl.getTable().getName());
-		       for (ColumnDefinition cdef : tbl.getColumnDefinitions()) {
-			      //System.out.println("+++ " + cdef.getColumnName());
-			      p.addAttribute(cdef.getColumnName());
-		       }
-		       schema.addPredicate(p);
-			}
-		    catch (ClassCastException c) { }
-		}
-		
+		readPredicateDefinitions(schemaString, schema);
+
 		System.out.println("filename;vertices;edges;degree;bip;b3ip;b4ip;vc");
-		
-		for (int i=1; i < args.length; i++) {
+
+		for (int i = 1; i < args.length; i++) {
 			File file = new File(args[i]);
 			File[] files;
 			if (file.isDirectory()) {
@@ -58,69 +40,91 @@ public class MainSQL {
 				files = new File[1];
 				files[0] = file;
 			}
-		    processFiles(files, schema);
+			processFiles(files, schema);
 		}
 	}
-	
+
 	private static void processFiles(File[] files, Schema schema) throws JSQLParserException {
 		for (File file : files) {
-	        if (file.isDirectory()) {
-	            //System.out.println("Directory: " + file.getName());
-	            processFiles(file.listFiles(), schema); // Calls same method again.
-	        } else if (file.getName().endsWith("sql")) {
-	        	
-			    HypergraphFromSQLHelper sql2hg = new HypergraphFromSQLHelper();
-			    String sqlString = readFile(file.getPath());
-			    Statement stmt = CCJSqlParserUtil.parse(sqlString);
-			
-			    Select selectStmt = (Select)stmt;
-			    HypergraphFromSQLFinder hgFinder = new HypergraphFromSQLFinder();
-			    hgFinder.run(schema, selectStmt);
-			    Map<String,String> tableList = hgFinder.getAtomList();
-			
-			    for (String s : tableList.keySet()) {
-			    	//System.out.println(s+": "+tableList.get(s));
-				    sql2hg.addAtom(schema.getPredicate(tableList.get(s)), s);
-			    }
-			
-			    for (SameColumn sc : hgFinder.getJoinList()) {
-			    	Column a = sc.getA();
-			    	Column b = sc.getB();
-			    	//System.out.println(a.getTable()+"."+a.getColumnName()+"="+b.getTable()+"."+b.getColumnName());
-			    	sql2hg.addJoin(sc);
-			    }
-			
-			
-			    Hypergraph H = sql2hg.getHypergraph();
-			
-			    System.out.print(file.getPath()+";");
-			    System.out.print(H.cntVertices()+";");
-			    System.out.print(H.cntEdges()+";");
-			    System.out.print(H.degree()+";");
-			    System.out.print(H.cntBip(2)+";");
-			    System.out.print(H.cntBip(3)+";");
-			    System.out.print(H.cntBip(4)+";");
-			    System.out.println(H.VCdimension());
-	        }
+			if (file.isDirectory()) {
+				// System.out.println("Directory: " + file.getName());
+				processFiles(file.listFiles(), schema); // Calls same method again.
+			} else if (file.getName().endsWith("sql") || file.getName().endsWith("tpl")) {
+				/*
+				 * TODO create a regex containing all possible extensions and then check if the
+				 * name matches it
+				 */
+				String sqlString = readFile(file.getPath());
+				Statement stmt = CCJSqlParserUtil.parse(sqlString);
+				Select selectStmt = (Select) stmt;
+				ConjunctiveQueryFinder hgFinder = new ConjunctiveQueryFinder(schema);
+				hgFinder.run(selectStmt);
+
+				HypergraphBuilder hgBuilder = new HypergraphBuilder();
+				for (Predicate table : hgFinder.getTables()) {
+					// System.out.println(table);
+					hgBuilder.buildEdge(table);
+				}
+				for (Predicate table : hgFinder.getTables()) {
+					if (table instanceof ViewPredicate) {
+						ViewPredicate view = (ViewPredicate) table;
+						// System.out.println(view);
+						for (Equality join : view.getJoins()) {
+							// System.out.println(join);
+							hgBuilder.buildViewJoin(view.getAlias(), join);
+						}
+					}
+				}
+				for (Equality join : hgFinder.getJoins()) {
+					// System.out.println(join);
+					hgBuilder.buildJoin(join);
+				}
+				Hypergraph h = hgBuilder.getHypergraph();
+
+				System.out.print(file.getPath() + ";");
+				System.out.print(h.cntVertices() + ";");
+				System.out.print(h.cntEdges() + ";");
+				System.out.print(h.degree() + ";");
+				System.out.print(h.cntBip(2) + ";");
+				System.out.print(h.cntBip(3) + ";");
+				System.out.print(h.cntBip(4) + ";");
+				System.out.println(h.VCdimension());
+			}
+		}
+	}
+
+	private static void readPredicateDefinitions(String schemaString, Schema schema) throws JSQLParserException {
+		Statements schemaStmts = CCJSqlParserUtil.parseStatements(schemaString);
+		for (Statement schemaStmt : schemaStmts.getStatements()) {
+			try {
+				CreateTable tbl = (CreateTable) schemaStmt;
+
+				// System.out.println("Table: "+tbl.getTable().getName());
+				String predicateName = tbl.getTable().getName();
+				LinkedList<String> attributes = new LinkedList<>();
+				for (ColumnDefinition cdef : tbl.getColumnDefinitions()) {
+					// System.out.println("+++ " + cdef.getColumnName());
+					attributes.add(cdef.getColumnName());
+				}
+				schema.addPredicateDefinition(new PredicateDefinition(predicateName, attributes));
+			} catch (ClassCastException c) {
+				System.err.println("\"" + schemaStmt + "\" is not a CREATE statement.");
+			}
 		}
 	}
 
 	public static String readFile(String fName) {
 		String s = "";
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(fName)))
-		{
+		try (BufferedReader br = new BufferedReader(new FileReader(fName))) {
 			String sCurrentLine;
-
 			while ((sCurrentLine = br.readLine()) != null) {
 				if (!sCurrentLine.startsWith("--"))
-				   s += sCurrentLine + " ";
+					s += sCurrentLine + " ";
 			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		return s;
 	}
+
 }
