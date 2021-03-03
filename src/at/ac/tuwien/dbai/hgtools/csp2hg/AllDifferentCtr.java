2212aa3239c6920e41606fa1cf2c72dc5c45376e
dmlongo <davide.longo@tuwien.ac.at>
2021-03-03 13:21:19 +0100

    AllDifferentCtr implemented for XCSP

---
+++ src/at/ac/tuwien/dbai/hgtools/csp2hg/AllDifferentCtr.java
@@ -0,0 +1,41 @@
+package at.ac.tuwien.dbai.hgtools.csp2hg;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AllDifferentCtr implements Constraint {
+
+    private ArrayList<String> vars;
+
+    public AllDifferentCtr(String[] vars) {
+        if (vars == null) {
+            throw new NullPointerException();
+        }
+        this.vars = new ArrayList<>(vars.length);
+        for (String v : vars) {
+            this.vars.add(v);
+        }
+    }
+
+    @Override
+    public List<String> toFile() {
+        ArrayList<String> out = new ArrayList<>(2);
+        out.add("AllDifferentCtr");
+        StringBuilder sb = new StringBuilder(5 * vars.size());
+        for (int i = 0; i < vars.size(); i++) {
+            sb.append(vars.get(i));
+            if (i < vars.size() - 1) {
+                sb.append(" ");
+            }
+        }
+        out.add(sb.toString());
+        return out;
+    }
+
+    @Override
+    public List<String> getVariables() {
+        return Collections.unmodifiableList(vars);
+    }
+
+}
